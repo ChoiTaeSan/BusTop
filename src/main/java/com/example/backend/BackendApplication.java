09550e9b3e5e32ca31cf66683549f6107e8e626d
Choi TaeSan <xotks7524@gmail.com>
2024-01-31 16:59:57 +0900

    first commit

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -3,11 +3,20 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+
+        double sum = A / B;
+
+        System.out.println(sum);
     }
 
 }
--- src/main/java/com/example/backend/테스트/Main.java
+++
@@ -1,4 +0,0 @@
-package com.example.backend.테스트;
-
-public class Main {
-}
